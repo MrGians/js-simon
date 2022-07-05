41adfcd01aa5163fbdbf928f924c75bdf4c5e450
MrGians <gianlumura97@gmail.com>
2022-07-05 13:08:13 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
-// Descrizione:
+// TRACCIA
 // Visualizzare in pagina 5 numeri casuali  diversi tra loro. Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite i prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-
-
-// OK 1 -Visualizzare 5 numeri casuali in pagina diversi tra di loro
-// OK 2 - Parte un timer di 30 secondi
-// OK 3 - Dopo i 30 secondi i numeri spariscono
-// OK 4 - Dopo la sparizione all'utente appariranno 5 prompt dove dovrà inserire i numeri precedentemente visti
-// 5 - Dopo l'inserimento dei numeri confrontare Numeri Casuali con Numeri Utente
-// 5.1 - Ogni corrispondenza genera 1 punto, fino ad un massimo di 5 punti
-// 6 - Stampare il risultato ottenuto dall'utente 
 
 
 // Dichiaro variabili DOM
